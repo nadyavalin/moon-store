1a95428a3f9e1025d4709462cf3bb8c212d21123
Darya Pchelintseva <raelin@mail.ru>
2024-05-08 23:11:52 +0300

    MS-17 feat(view): create view for login page

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import "./index.css";
 import "./pages/mainPage/mainPage.css";
 import "./api/api";
-import "./pages/loginPage/loginPage";
-import { header, main, footer } from "./pages/mainPage/mainPage";
 
-document.body.append(header, main, footer);
+// import { header, main, footer } from "./pages/mainPage/mainPage";
+
+// document.body.append(header, main, footer);
+
+import formWrapper from "./pages/loginPage/loginPage";
+
+document.body.append(formWrapper);
--- src/pages/loginPage/loginPage.ts
+++ src/pages/loginPage/loginPage.ts
@@ -1,1 +1,18 @@
 import "./loginPage.css";
+
+import { createElement, createInput } from "src/components/elements";
+
+const formWrapper = <HTMLDivElement>createElement("div", ["loginForm-wrapper"]);
+const formInner = <HTMLDivElement>createElement("div", ["loginForm-inner"]);
+const h1 = <HTMLHeadingElement>createElement("h2", ["loginForm-heading"], "Авторизация");
+const form = <HTMLFormElement>createElement("form", ["loginForm"]);
+const emailInput = <HTMLInputElement>createInput("email", "email", ["email-input"], "Email");
+const passwordInput = <HTMLInputElement>createInput("password", "text", ["password-input"], "Пароль");
+const showPassword = <HTMLDivElement>createElement("div", ["showPassword"]);
+showPassword.innerHTML = `<label><input type="checkbox"></input>Показать пароль</label>`;
+
+form.append(emailInput, passwordInput);
+formInner.append(h1, form, showPassword);
+formWrapper.append(formInner);
+
+export default formWrapper;
