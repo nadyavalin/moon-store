2919743dacc71f69957a7cb47c29a0a9c16add63
ifbfirst <60338444+ifbfirst@users.noreply.github.com>
2024-05-09 13:45:55 +0300

    MS-25 feat(registration-form): implement registration form and checking validity form fields
    * feat: implement view for form registartion
    
    * feat: implement patterns for fields
    
    * feat: implement patterns for fields
    
    * feat: implement validity form and messages
    
    * feat: implement validity form and messages
    
    * feat: change color for form text
    
    * feat: change color for form text
    
    * feat(registration-form): add validity for all fields
    
    * fix(registration-form): fix name of function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import "./index.css";
 import "./pages/mainPage/mainPage.css";
 import "./api/api";
+
 import { header, main, footer } from "./pages/mainPage/mainPage";
 
 document.body.append(header, main, footer);
+
+// import { createFormRegistration } from "./pages/registration/registrationView";
+
+// const formRegistration = createFormRegistration();
+// document.body.append(formRegistration);
