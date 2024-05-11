0714c60f5b38477c97fa8310f8d9a62ea84422d3
Darya Pchelintseva <raelin@mail.ru>
2024-05-11 14:06:31 +0300

    MS-17 fix(classes): fix classes names

--- src/pages/loginPage/loginPage.ts
+++ src/pages/loginPage/loginPage.ts
@@ -1,10 +1,10 @@
 import "./loginPage.css";
 
-import { createElement, createInput, createLink } from "src/components/elements";
+import { createElement, createInput, createLink, createSubmitButton } from "src/components/elements";
 import validateListener from "../registration/checkValidityForm";
 
 const emailPattern = "([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+.[a-zA-Z0-9_-]+)";
-const emailTitle = "Email должен быть в формате example@gmail.com";
+const emailTitle = "Email должен быть в формате example@example.ru";
 const passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}";
 const passwordTitle = "Пароль должен содержать 8 символов и включать 1 цифру, 1 заглавную и 1 строчную латинские буквы";
 
@@ -21,28 +21,26 @@ const showHidePasswordHandler = (togglePassword: HTMLInputElement, passwordInput
 };
 
 function renderLoginFormContent(): HTMLElement {
-  const formInner = <HTMLDivElement>createElement("div", ["loginForm-inner"]);
-  const h1 = <HTMLHeadingElement>createElement("h2", ["loginForm-heading"], "Авторизация");
-  const form = <HTMLFormElement>createElement("form", ["loginForm"]);
+  const loginFormInner = <HTMLDivElement>createElement("div", ["login-form__inner"]);
+  const loginFormHeading = <HTMLHeadingElement>createElement("h2", ["login-form__heading"], "Авторизация");
+  const loginForm = <HTMLFormElement>createElement("form", ["login-form"]);
   const emailInput = <HTMLInputElement>createInput("email", "email", ["email-input"], "Email", emailPattern, emailTitle);
   const passwordInput = <HTMLInputElement>createInput("password", "password", ["password-input"], "Пароль", passwordPattern, passwordTitle);
-  const submit = <HTMLInputElement>createInput("submit", "submit", ["form-submit", "disabled"]);
-  submit.removeAttribute("required");
-  submit.disabled = true;
-  const showPassword = <HTMLDivElement>createElement("div", ["showPassword"]);
-  const togglePassword = <HTMLInputElement>createInput("checkbox", "checkbox", ["togglePassword"]);
+  const loginFormSubmitButoon = <HTMLButtonElement>createSubmitButton("Отправить");
+  const showPasswordArea = <HTMLDivElement>createElement("div", ["login-form__show-password"]);
+  const togglePassword = <HTMLInputElement>createInput("checkbox", "checkbox", ["login-form__password-toggle"]);
   togglePassword.removeAttribute("required");
-  showPassword.append(togglePassword, `Показать пароль`);
+  showPasswordArea.append(togglePassword, `Показать пароль`);
   const linkToRegistration = <HTMLAnchorElement>createLink("#registration", ["registration-link"], "Еще не зарегистрированы? Регистрация...");
 
   showHidePasswordHandler(togglePassword, passwordInput);
-  validateListener(emailInput, emailTitle, passwordInput, form, emailPattern);
-  validateListener(passwordInput, passwordTitle, showPassword, form, passwordPattern);
+  validateListener(emailInput, emailTitle, passwordInput, loginForm, emailPattern);
+  validateListener(passwordInput, passwordTitle, showPasswordArea, loginForm, passwordPattern);
 
-  form.append(emailInput, passwordInput, showPassword, submit);
-  formInner.append(h1, form, linkToRegistration);
+  loginForm.append(emailInput, passwordInput, showPasswordArea, loginFormSubmitButoon);
+  loginFormInner.append(loginFormHeading, loginForm, linkToRegistration);
 
-  return formInner;
+  return loginFormInner;
 }
 
 export default renderLoginFormContent;
