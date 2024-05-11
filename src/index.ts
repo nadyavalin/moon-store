926c4e86375ebaca25aa1ca1fc61ec0cc9e1f745
Darya Pchelintseva <126498926+raenlin@users.noreply.github.com>
2024-05-11 15:08:51 +0300

    MS-17 feat(login-form): implement login form and navigation to registration page
    * MS-17 feat(files): create files for loginPage
    
    * MS-17 feat(view): create view for login page
    
    * MS-17 feat(validation): implement client-side validation for the login form
    
    * MS-17 feat(messages):display clear error messages for validation issues
    
    * MS-17 feat(navigate): add a button to navigate to the registration page
    
    * MS-17 fix(classes): fix classes names

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import "./index.css";
 import "./pages/basePage/basePage.css";
 import "./pages/404/404.css";
 import "./api/api";
+
 import { header, main, footer } from "./pages/basePage/basePage";
 import { renderMainPageContent } from "./pages/main/main";
 import { renderCatalogContent } from "./pages/catalog/catalog";
 import { renderBasketContent } from "./pages/basket/basket";
 import { renderAboutUsContent } from "./pages/about/about";
-import { renderLoginFormContent } from "./pages/loginForm/loginForm";
+import renderLoginFormContent from "./pages/loginPage/loginPage";
 import { renderRegistrationFormContent } from "./pages/registration/registrationView";
 import { render404PageContent } from "./pages/404/404";
 
@@ -35,6 +36,7 @@ function navigate() {
   const contentDiv = document.querySelector(".main");
   const fragmentId = window.location.hash.substring(1);
   if (contentDiv) {
+    contentDiv.innerHTML = "";
     switch (fragmentId) {
       case "":
       case "main":
@@ -50,7 +52,7 @@ function navigate() {
         contentDiv.innerHTML = renderAboutUsContent();
         break;
       case "login":
-        contentDiv.innerHTML = renderLoginFormContent();
+        contentDiv.append(renderLoginFormContent());
         break;
       case "registration":
         contentDiv.innerHTML = renderRegistrationFormContent().outerHTML;
---
+++ src/pages/loginPage/loginPage.ts
@@ -0,0 +1,46 @@
+import "./loginPage.css";
+
+import { createElement, createInput, createLink, createSubmitButton } from "src/components/elements";
+import validateListener from "../registration/checkValidityForm";
+
+const emailPattern = "([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+.[a-zA-Z0-9_-]+)";
+const emailTitle = "Email должен быть в формате example@example.ru";
+const passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}";
+const passwordTitle = "Пароль должен содержать 8 символов и включать 1 цифру, 1 заглавную и 1 строчную латинские буквы";
+
+const showHidePasswordHandler = (togglePassword: HTMLInputElement, passwordInput: HTMLInputElement) => {
+  const toggle = togglePassword;
+  const password = passwordInput;
+  toggle.onchange = () => {
+    if (toggle.checked) {
+      password.type = "text";
+    } else {
+      password.type = "password";
+    }
+  };
+};
+
+function renderLoginFormContent(): HTMLElement {
+  const loginFormInner = <HTMLDivElement>createElement("div", ["login-form__inner"]);
+  const loginFormHeading = <HTMLHeadingElement>createElement("h2", ["login-form__heading"], "Авторизация");
+  const loginForm = <HTMLFormElement>createElement("form", ["login-form"]);
+  const emailInput = <HTMLInputElement>createInput("email", "email", ["email-input"], "Email", emailPattern, emailTitle);
+  const passwordInput = <HTMLInputElement>createInput("password", "password", ["password-input"], "Пароль", passwordPattern, passwordTitle);
+  const loginFormSubmitButoon = <HTMLButtonElement>createSubmitButton("Отправить");
+  const showPasswordArea = <HTMLDivElement>createElement("div", ["login-form__show-password"]);
+  const togglePassword = <HTMLInputElement>createInput("checkbox", "checkbox", ["login-form__password-toggle"]);
+  togglePassword.removeAttribute("required");
+  showPasswordArea.append(togglePassword, `Показать пароль`);
+  const linkToRegistration = <HTMLAnchorElement>createLink("#registration", ["registration-link"], "Еще не зарегистрированы? Регистрация...");
+
+  showHidePasswordHandler(togglePassword, passwordInput);
+  validateListener(emailInput, emailTitle, passwordInput, loginForm, emailPattern);
+  validateListener(passwordInput, passwordTitle, showPasswordArea, loginForm, passwordPattern);
+
+  loginForm.append(emailInput, passwordInput, showPasswordArea, loginFormSubmitButoon);
+  loginFormInner.append(loginFormHeading, loginForm, linkToRegistration);
+
+  return loginFormInner;
+}
+
+export default renderLoginFormContent;
