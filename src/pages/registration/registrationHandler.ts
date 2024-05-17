487b9c7be269217373daf470972367a5cf04a0f5
Kate Mashko <kat_zb@mail.ru>
2024-05-17 09:28:42 +0300

    MS-30 feat(registration-form): add block checkbox for default address

--- src/pages/registration/registrationHandler.ts
+++ src/pages/registration/registrationHandler.ts
@@ -2,45 +2,54 @@ import createCustomer from "src/api/api";
 import { createSnackbar } from "src/components/elements";
 import { authorizeUserWithToken } from "../loginPage/loginHandler";
 
-export function formRegistrationHandler() {
+export function formRegistrationHandler(event: Event) {
   const form = <HTMLFormElement>document.querySelector(".registration-form");
-  form.addEventListener("click", (event) => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const email = <string>formData.get("email");
-    const password = <string>formData.get("password");
-    const name = <string>formData.get("name");
-    const surname = <string>formData.get("surname");
-    const birthday = <string>formData.get("birthday");
-    const city = <string>formData.get("city");
-    const streetName = <string>formData.get("street");
-    const postalCode = <string>formData.get("index");
-    const countries = <HTMLSelectElement>document.querySelector(".countries");
-    const country = countries.value;
+  event.preventDefault();
+  const formData = new FormData(form);
+  const email = <string>formData.get("email");
+  const password = <string>formData.get("password");
+  const name = <string>formData.get("name");
+  const surname = <string>formData.get("surname");
+  const birthday = <string>formData.get("birthday");
+  const city = <string>formData.get("city");
+  const streetName = <string>formData.get("street");
+  const postalCode = <string>formData.get("index");
+  const countries = <HTMLSelectElement>document.querySelector(".countries");
+  const country = countries.value;
+  // const checkboxDefault = <HTMLInputElement>document.querySelector(".checkbox");
 
-    const requestBody = {
-      email: email.trim(),
-      password: password.trim(),
-      firstName: name.trim(),
-      lastName: surname.trim(),
-      dateOfBirth: birthday,
-      addresses: [{ country, city, streetName, postalCode }],
-    };
+  // if(checkboxDefault.checked)
 
-    createCustomer(requestBody)
-      .then(({ body }) => {
-        createSnackbar(`Пользователь ${body.customer.firstName} создан`);
-        authorizeUserWithToken(email.trim(), password.trim());
+  const requestBody = {
+    email: email.trim(),
+    password: password.trim(),
+    firstName: name.trim(),
+    lastName: surname.trim(),
+    dateOfBirth: birthday,
+    addresses: [{ country, city, streetName, postalCode }],
+    defaultShippingAddress: 0,
+    defaultBillingAddress: 0,
+    shippingAddresses: [0],
+    billingAddresses: [0],
+  };
+
+  // requestBody.billingAddresses = [0]
+
+  createCustomer(requestBody)
+    .then((response) => {
+      if (response.statusCode === 201) {
+        createSnackbar(`Пользователь ${response.body.customer.firstName} создан`);
+        setTimeout(() => authorizeUserWithToken(email.trim(), password.trim()), 6000);
         window.location.href = "#main";
-      })
-      .catch(({ statusCode }) => {
-        if (statusCode === 400) {
-          createSnackbar(`Пользователь c таким адресом электронной почты уже существует. Войдите в приложение`);
-        }
-        if (statusCode === 500) {
-          createSnackbar(`Что-то пошло не так... Попробуйте зарегистрироваться позже`);
-        }
-      });
-  });
+      }
+    })
+    .catch(({ statusCode }) => {
+      if (statusCode === 400) {
+        createSnackbar(`Пользователь c таким адресом электронной почты уже существует. Войдите в приложение`);
+      }
+      if (statusCode === 500) {
+        createSnackbar(`Что-то пошло не так... Попробуйте зарегистрироваться позже`);
+      }
+    });
 }
 export default formRegistrationHandler;
