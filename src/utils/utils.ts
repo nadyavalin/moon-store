c875549e0a0f4a470ce92d22ce25b233f86dd32a
Darya Pchelintseva <raelin@mail.ru>
2024-05-18 16:07:57 +0300

    MS-59 fix(func): delete unnecessary funcs

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,17 +1,5 @@
 export const priceFormatter = new Intl.NumberFormat("ru-RU", { style: "currency", currency: "RUB", minimumFractionDigits: 0 });
 
-// export function setItemToLocalStorage(key: string, value: string) {
-//   localStorage.setItem(key, value);
-// }
-
-// export function getItemFromLocalStorage(key: string) {
-//   if (localStorage.getItem(key)) {
-//     const item = localStorage.getItem(key);
-//     return item;
-//   }
-//   return null;
-// }
-
 export function setItemToLocalStorage<T>(key: string, value: T) {
   localStorage.setItem(key, JSON.stringify({ value }));
 }
