2595397e15974f71cdd455c57d6cf6d168bbdb69
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-05-26 15:36:07 +0300

    MS-76 refactor(prices): return priceFormatter for prices and discounts, css styles refactoring

--- src/components/productCard.ts
+++ src/components/productCard.ts
@@ -2,6 +2,7 @@ import "./productCard.css";
 
 import { createElement } from "./elements";
 import { ProductProjection } from "@commercetools/platform-sdk";
+import priceFormatter from "../utils/utils";
 
 export function createCard(item: ProductProjection) {
   const name = item.name.ru;
@@ -12,26 +13,26 @@ export function createCard(item: ProductProjection) {
   const card = createElement({ tagName: "li", classNames: ["card"] });
   const cardBottom = createElement({ tagName: "div", classNames: ["card__bottom-wrapper"] });
   const cardTextWrapper = createElement({ tagName: "div", classNames: ["card__text-wrapper"] });
-  const cardname = createElement({ tagName: "h3", classNames: ["card__name"], textContent: name });
+  const cardName = createElement({ tagName: "h3", classNames: ["card__name"], textContent: name });
   const cardDescription = createElement({ tagName: "div", classNames: ["card__description"], textContent: description });
   const cardPrices = createElement({ tagName: "div", classNames: ["card__prices"] });
   const cardButton = createElement({ tagName: "button", classNames: ["card__button"], textContent: "Добавить в корзину" });
 
-  if (images) {
-    const cardImage = createElement({ tagName: "img", classNames: ["card__img"], attributes: { src: `${images[0].url}`, alt: "Photo" } });
-    cardImage.setAttribute("draggable", "false");
-    card.append(cardImage);
-  }
+  const cardImage = createElement({ tagName: "img", classNames: ["card__img"], attributes: { src: `${images?.[0].url}`, alt: "Photo" } });
+  cardImage.setAttribute("draggable", "false");
+  card.append(cardImage);
 
-  if (prices) {
-    const priceAmount = String(prices[0].value.centAmount).slice(0, -2);
-    const discountAmount = String(prices[0].discounted?.value.centAmount).slice(0, -2);
-    const price = createElement({ tagName: "p", classNames: ["card__price"], textContent: `${priceAmount} ₽` });
-    const discount = createElement({ tagName: "p", classNames: ["card__discount"], textContent: `${discountAmount} ₽` });
-    cardPrices.append(price, discount);
-  }
+  const priceAmount = (prices?.[0].value.centAmount ?? 0) / 100;
+  const discountAmount = (prices?.[0].discounted?.value.centAmount ?? 0) / 100;
+  const price = createElement({ tagName: "p", classNames: ["card__price"], textContent: priceFormatter.format(priceAmount) });
+  const discount = createElement({
+    tagName: "p",
+    classNames: ["card__discount"],
+    textContent: priceFormatter.format(discountAmount),
+  });
+  cardPrices.append(price, discount);
 
-  cardTextWrapper.append(cardname, cardDescription);
+  cardTextWrapper.append(cardName, cardDescription);
   cardBottom.append(cardPrices, cardButton);
   card.append(cardTextWrapper, cardBottom);
   return card;
