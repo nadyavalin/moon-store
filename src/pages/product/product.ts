b4e2de19c2d4f3b827653fd785d15c0ab6ad2f72
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-05-27 21:48:34 +0300

    MS-81 feat(product page styles): correct and add styles to product page

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -6,6 +6,7 @@ import { ProductProjection, ClientResponse } from "@commercetools/platform-sdk";
 export const productWrapper = createElement({ tagName: "div", classNames: ["product__wrapper"] });
 
 export function renderProductContent(response: ClientResponse<ProductProjection>) {
+  productWrapper.innerHTML = "";
   const productInner = productWrapper.querySelectorAll("div");
   productInner.forEach((item) => item.remove());
 
@@ -15,31 +16,49 @@ export function renderProductContent(response: ClientResponse<ProductProjection>
   const images = response.body.masterVariant.images;
   const productSizes = response.body.variants;
 
-  const productTextWrapper = createElement({ tagName: "div", classNames: ["product__text-wrapper"] });
-  const imageContainer = createElement({ tagName: "div" });
-  const name = createElement({ tagName: "h4", classNames: ["product__name"], textContent: `${cardName}` });
-  const description = createElement({ tagName: "p", classNames: ["product__description"], textContent: `${cardDescription}` });
-  const size = createElement({ tagName: "div", classNames: ["product__size"], textContent: "Размеры:" });
+  const productTextButtonWrapper = createElement({ tagName: "div", classNames: ["product__text-button-wrapper"] });
+  const textWrapper = createElement({ tagName: "div", classNames: ["product__text-wrapper"] });
+  const imageContainer = createElement({ tagName: "a", classNames: ["image__link"], attributes: { href: `#` } });
+  const name = createElement({ tagName: "p", classNames: ["product__name"], innerHTML: `<b>Название:</b> ${cardName}` });
+  const description = createElement({ tagName: "p", classNames: ["product__description"], innerHTML: `<b>Описание:</b> ${cardDescription}` });
+  const size = createElement({ tagName: "div", classNames: ["product__size"], innerHTML: "<b>Размеры:</b> " });
+
+  const pricesWrapper = createElement({ tagName: "div", classNames: ["prices__wrapper"] });
+  const priceWrapper = createElement({ tagName: "div", classNames: ["price__wrapper"] });
+  const priceText = createElement({ tagName: "p", classNames: ["price-text"], textContent: "Цена: " });
   const price = createElement({
     tagName: "p",
     classNames: ["product__price"],
     textContent: PriceFormatter.formatCents(cardPrices?.[0].value.centAmount),
   });
+  const discountWrapper = createElement({ tagName: "div", classNames: ["discount__wrapper"] });
+  const discountText = createElement({ tagName: "p", classNames: ["discount-text"], textContent: "Скидка: " });
   const discount = createElement({
     tagName: "p",
     classNames: ["product__discount"],
     textContent: PriceFormatter.formatCents(cardPrices?.[0].discounted?.value.centAmount),
   });
+
   const buyButton = createElement({ tagName: "button", classNames: ["product__buy-button"], textContent: "Добавить в корзину" });
-  images?.forEach((img) => {
-    const image = createElement({ tagName: "img", classNames: ["product__img"], attributes: { src: `${img.url}` } });
-    imageContainer.append(image);
-  });
+
+  // TODO Use this code in product slider
+  // images?.forEach((img) => {
+  //   const image = createElement({ tagName: "img", classNames: ["product__img"], attributes: { src: `${img.url}` } });
+  //   imageContainer.append(image);
+  // });
+
+  const image = createElement({ tagName: "img", classNames: ["product__img"], attributes: { src: `${images?.[0].url}` } });
+  imageContainer.append(image);
+
   productSizes.forEach((variant) => {
     const sizeItem = createElement({ tagName: "span", classNames: ["product__size-item"], textContent: `${variant.sku?.slice(-1)}` });
     size.append(sizeItem);
   });
 
-  productTextWrapper.append(name, description, size, price, discount, buyButton);
-  productWrapper.append(imageContainer, productTextWrapper);
+  priceWrapper.append(priceText, price);
+  discountWrapper.append(discountText, discount);
+  pricesWrapper.append(priceWrapper, discountWrapper);
+  textWrapper.append(name, description, size, pricesWrapper);
+  productTextButtonWrapper.append(textWrapper, buyButton);
+  productWrapper.append(imageContainer, productTextButtonWrapper);
 }
