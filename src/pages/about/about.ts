fb530a0b1d383b1c80c798a1a5a51a5a7f4234cc
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-06-08 18:40:59 +0300

    MS-149 feat(about us page): add info for About Us Page, correct the layout of the product card on the basket page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,10 +1,109 @@
+import "./about.css";
+import { createElement } from "../../components/elements";
+import { introduction1p, introduction2p, introduction3p, introduction4p, katikaBioText, nadyavalinBioText, raenlinBioText } from "./text";
+
 export function renderAboutUsContent() {
-  const aboutUsTemporaryContent = "This is the About Us page!";
-  return `
-      <div class="about-us-page">
-          ${aboutUsTemporaryContent}
-      </div>
-    `;
+  const aboutUsSchoolLogo = createElement({
+    tagName: "img",
+    classNames: ["rs-school-logo", "about-us__rs-school-logo"],
+    attributes: { src: "../../public/img/rs-school-logo.png", alt: "RSSchool Logo" },
+  });
+  const aboutUsSchoolLogoLink = createElement({ tagName: "a", attributes: { href: "https://rs.school/courses", target: "_blank" } });
+
+  const aboutUsWrapper = createElement({ tagName: "div", classNames: ["about-us-wrapper"] });
+  const introductionWrapper = createElement({ tagName: "div", classNames: ["intro-wrapper"] });
+  const introductionFirstWrapper = createElement({ tagName: "div", classNames: ["intro-first-wrapper"] });
+  const introductionSecondWrapper = createElement({ tagName: "div", classNames: ["intro-second-wrapper"] });
+  const introduction1Paragraph = createElement({ tagName: "p", classNames: ["intro-paragraph"], textContent: introduction1p });
+  const introduction2Paragraph = createElement({ tagName: "p", classNames: ["intro-paragraph"], textContent: introduction2p });
+  const introduction3Paragraph = createElement({ tagName: "p", classNames: ["intro-paragraph"], textContent: introduction3p });
+  const introduction4Paragraph = createElement({ tagName: "p", classNames: ["intro-paragraph"], textContent: introduction4p });
+  const developersWrapper = createElement({ tagName: "div", classNames: ["developers-wrapper"] });
+  const nadyavalinWrapper = createElement({ tagName: "div", classNames: ["nadyavalin-wrapper"] });
+  const raenlinWrapper = createElement({ tagName: "div", classNames: ["raenlin-wrapper"] });
+  const katikaWrapper = createElement({ tagName: "div", classNames: ["katika-wrapper"] });
+
+  const nadyavalinPhoto = createElement({
+    tagName: "img",
+    classNames: ["developers-image"],
+    attributes: { src: "../../public/developers-img/nadyavalin.jpg" },
+  });
+  const raenlinPhoto = createElement({
+    tagName: "img",
+    classNames: ["developers-image"],
+    attributes: { src: "../../public/developers-img/raenlin.jpg" },
+  });
+  const katikaPhoto = createElement({
+    tagName: "img",
+    classNames: ["developers-image"],
+    attributes: { src: "../../public/developers-img/ifbfirst.jpg" },
+  });
+
+  const nadyavalinName = createElement({ tagName: "p", classNames: ["developers-name"], textContent: "Надежда Ткачук" });
+  const raenlinName = createElement({ tagName: "p", classNames: ["developers-name"], textContent: "Дарья Пчелинцева" });
+  const katikaName = createElement({ tagName: "p", classNames: ["developers-name"], textContent: "Екатерина Машко" });
+
+  const nadyavalinPosition = createElement({
+    tagName: "p",
+    classNames: ["developers-position"],
+    textContent: "Front-end developer",
+  });
+  const raenlinPosition = createElement({
+    tagName: "p",
+    classNames: ["developers-position"],
+    textContent: "Front-end developer",
+  });
+  const katikaPosition = createElement({
+    tagName: "p",
+    classNames: ["developers-position"],
+    textContent: "Front-end developer",
+  });
+
+  const nadyavalinGitHub = createElement({
+    tagName: "a",
+    classNames: ["developers-github-link"],
+    textContent: "GitHub: nadyavalin",
+    attributes: { href: "https://github.com/nadyavalin", target: "_blank" },
+  });
+  const raenlinGitHub = createElement({
+    tagName: "a",
+    classNames: ["developers-github-link"],
+    textContent: "GitHub: raenlin",
+    attributes: { href: "https://github.com/raenlin", target: "_blank" },
+  });
+  const katikaGitHub = createElement({
+    tagName: "a",
+    classNames: ["developers-github-link"],
+    textContent: "GitHub: ifbfirst",
+    attributes: { href: "https://github.com/ifbfirst", target: "_blank" },
+  });
+
+  const nadyavalinBio = createElement({
+    tagName: "p",
+    classNames: ["developers-bio"],
+    textContent: nadyavalinBioText,
+  });
+  const raenlinBio = createElement({
+    tagName: "p",
+    classNames: ["developers-bio"],
+    textContent: raenlinBioText,
+  });
+  const katikaBio = createElement({
+    tagName: "p",
+    classNames: ["developers-bio"],
+    textContent: katikaBioText,
+  });
+
+  aboutUsSchoolLogoLink.append(aboutUsSchoolLogo);
+  nadyavalinWrapper.append(nadyavalinPhoto, nadyavalinName, nadyavalinPosition, nadyavalinGitHub, nadyavalinBio);
+  raenlinWrapper.append(raenlinPhoto, raenlinName, raenlinPosition, raenlinGitHub, raenlinBio);
+  katikaWrapper.append(katikaPhoto, katikaName, katikaPosition, katikaGitHub, katikaBio);
+  developersWrapper.append(nadyavalinWrapper, raenlinWrapper, katikaWrapper);
+  introductionFirstWrapper.append(introduction1Paragraph, introduction2Paragraph);
+  introductionSecondWrapper.append(introduction3Paragraph, introduction4Paragraph);
+  introductionWrapper.append(introductionFirstWrapper, introductionSecondWrapper);
+  aboutUsWrapper.append(introductionWrapper, aboutUsSchoolLogoLink, developersWrapper);
+  return aboutUsWrapper;
 }
 
 export default renderAboutUsContent;
--- src/pages/basket/basket.ts
+++ src/pages/basket/basket.ts
@@ -1,6 +1,6 @@
 import "./basket.css";
 import { createElement } from "../../components/elements";
-import { createBusketCard } from "./productBasketCard/productBasketCard";
+import { createBasketCard } from "./productBasketCard/productBasketCard";
 
 export function renderBasketContent() {
   const basketWrapper = createElement({ tagName: "div", classNames: ["basket__wrapper"] });
@@ -8,17 +8,17 @@ export function renderBasketContent() {
   const productTotalWrapper = createElement({ tagName: "div", classNames: ["product-total__wrapper"] });
 
   const productTotalTextWrapper = createElement({ tagName: "div", classNames: ["product-total__text-wrapper"] });
-  const procutTotalTitle = createElement({ tagName: "p", classNames: ["product-total__title"], textContent: "Итого:" });
+  const productTotalTitle = createElement({ tagName: "p", classNames: ["product-total__title"], textContent: "Итого:" });
   const productTotalPrice = createElement({ tagName: "p", classNames: ["product-total__price"], textContent: "3400 р." });
 
   const productAmountTextWrapper = createElement({ tagName: "div", classNames: ["product-amount__wrapper"] });
-  const procutAmountTitle = createElement({ tagName: "p", classNames: ["product-amount__title"], textContent: "Всего товаров:" });
+  const productAmountTitle = createElement({ tagName: "p", classNames: ["product-amount__title"], textContent: "Всего товаров:" });
   const productFullAmount = createElement({ tagName: "p", classNames: ["product-amount__full-amount"], textContent: "2 шт." });
 
-  productListWrapper.append(createBusketCard());
+  productListWrapper.append(createBasketCard());
   productTotalWrapper.append(productTotalTextWrapper, productAmountTextWrapper);
-  productAmountTextWrapper.append(procutAmountTitle, productFullAmount);
-  productTotalTextWrapper.append(procutTotalTitle, productTotalPrice);
+  productAmountTextWrapper.append(productAmountTitle, productFullAmount);
+  productTotalTextWrapper.append(productTotalTitle, productTotalPrice);
   basketWrapper.append(productListWrapper, productTotalWrapper);
 
   return basketWrapper;
