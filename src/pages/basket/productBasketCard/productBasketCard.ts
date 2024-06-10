b78b30e8509fa3b1ed541350aa02faa13c2b5919
Nadezhda Tkachuk <125882223+nadyavalin@users.noreply.github.com>
2024-06-10 11:46:32 +0300

    MS-149 feat(about us page): add info for About Us Page
    * MS-149 feat(about us page): add info for About Us Page, correct the layout of the product card on the basket page
    
    * MS-149 fix(image): remove extra image
    
    * MS-149 fix(about us): fix intro about Dasha
    
    * MS-149 fix(links): fix links in app everywhere and on page About us

--- src/pages/basket/productBasketCard/productBasketCard.ts
+++ src/pages/basket/productBasketCard/productBasketCard.ts
@@ -2,55 +2,61 @@ import "./productBasketCard.css";
 import "../../product/product.css";
 import { createElement } from "../../../components/elements";
 
-export function createBusketCard() {
+export function createBasketCard() {
   const productBasketItem = createElement({ tagName: "li", classNames: ["product-basket-item"] });
   const productBasketImage = createElement({
     tagName: "img",
     classNames: ["product-basket__image"],
-    attributes: { src: "../../../public/img/img-1.png" },
+    attributes: { src: "../../../../public/img/img-1.png" },
   });
   const productBasketDeleteIconWrapper = createElement({
     tagName: "div",
-    classNames: ["product-busket__delete-button-wrapper"],
+    classNames: ["product-basket__delete-button-wrapper"],
     attributes: { title: "Удалить из корзины" },
   });
-  const productBasketDeleteIcon = createElement({ tagName: "i", classNames: ["fa-solid", "fa-trash", "trash-button"] });
-  const productBasketTextWrapper = createElement({ tagName: "div", classNames: ["product-busket__text-wrapper"] });
-  const productBasketName = createElement({ tagName: "p", classNames: ["product-busket__name"], textContent: `Название: Футболки "Лёд и пламя"` });
-  const productBasketAmountSizeWrapper = createElement({ tagName: "div", classNames: ["product-busket__amount-sizes-wrapper"] });
-  const productBasketPricesWrapper = createElement({ tagName: "div", classNames: ["product-busket__prices-wrapper"] });
-  const prodcutBasketAmountWrapper = createElement({ tagName: "div", classNames: ["product-busket__amount-wrapper"] });
-  const productBasketAmountText = createElement({ tagName: "p", classNames: ["product-busket__small-text"], textContent: "Количество: " });
-  const proudctBasketAmountWrapper = createElement({ tagName: "div", classNames: ["product-busket__amount-wrapper"] });
-  const prodcutBasketPlusAmountButton = createElement({ tagName: "i", classNames: ["fa-solid", "fa-plus", "amount-button"] });
-  const productBasketAmount = createElement({ tagName: "p", classNames: ["product-busket__amount"], textContent: `2` });
-  const prodcutBasketMinusAmountButton = createElement({ tagName: "i", classNames: ["fa-solid", "fa-minus", "amount-button"] });
-  const productBasketSizeWrapper = createElement({ tagName: "div", classNames: ["product-busket__size-wrapper"] });
-  const productBasketSizeText = createElement({ tagName: "p", classNames: ["product-busket__small-text"], textContent: "Размер: " });
-  const productBasketSize = createElement({ tagName: "span", classNames: ["product__size-item"], textContent: `XL` });
-  const productBasketPriceWrapper = createElement({ tagName: "div", classNames: ["product-busket__price-wrapper"] });
-  const productBasketPriceText = createElement({ tagName: "p", classNames: ["product-busket__small-text"], textContent: "Цена за единицу: " });
-  const productBasketPrice = createElement({ tagName: "p", classNames: ["product-busket__price"], textContent: `2000 р.` });
-  const productBasketDiscoutWrapper = createElement({ tagName: "div", classNames: ["product-busket__discout-wrapper"] });
-  const productBasketDiscoutText = createElement({ tagName: "p", classNames: ["product-busket__small-text"], textContent: "Со скидкой: " });
-  const productBasketDiscout = createElement({ tagName: "p", classNames: ["product-busket__discount"], textContent: `1700 р.` });
 
-  const productBasketFinalPriceWrapper = createElement({ tagName: "div", classNames: ["product-busket__final-price-wrapper"] });
+  const productBasketDeleteIcon = createElement({ tagName: "i", classNames: ["fa-solid", "fa-trash", "trash-button"] });
+  const productBasketTextWrapper = createElement({ tagName: "div", classNames: ["product-basket__text-wrapper"] });
+  const productBasketName = createElement({ tagName: "p", classNames: ["product-basket__name"], textContent: `Название: Футболки "Лёд и пламя"` });
+
+  const productBasketAmountSizeWrapper = createElement({ tagName: "div", classNames: ["product-basket__amount-sizes-wrapper"] });
+  const productBasketAmountWrapper = createElement({ tagName: "div", classNames: ["product-basket__amount-wrapper"] });
+  const productBasketAmountText = createElement({ tagName: "p", classNames: ["product-basket__small-text"], textContent: "Количество: " });
+  const productBasketAmountButtonsWrapper = createElement({ tagName: "div", classNames: ["product-basket__amount-buttons-wrapper"] });
+
+  const productBasketPlusAmountButton = createElement({ tagName: "i", classNames: ["fa-solid", "fa-plus", "amount-button"] });
+  const productBasketAmount = createElement({ tagName: "p", classNames: ["product-basket__amount"], textContent: `2` });
+  const productBasketMinusAmountButton = createElement({ tagName: "i", classNames: ["fa-solid", "fa-minus", "amount-button"] });
+
+  const productBasketSizeWrapper = createElement({ tagName: "div", classNames: ["product-basket__size-wrapper"] });
+  const productBasketSizeText = createElement({ tagName: "p", classNames: ["product-basket__small-text"], textContent: "Размер: " });
+  const productBasketSize = createElement({ tagName: "span", classNames: ["product__size-item"], textContent: `XL` });
+
+  const productBasketPricesWrapper = createElement({ tagName: "div", classNames: ["product-basket__prices-wrapper"] });
+  const productBasketPriceWrapper = createElement({ tagName: "div", classNames: ["product-basket__price-wrapper"] });
+  const productBasketPriceText = createElement({ tagName: "p", classNames: ["product-basket__small-text"], textContent: "Цена за единицу: " });
+  const productBasketPrice = createElement({ tagName: "p", classNames: ["product-basket__price"], textContent: `2000 р.` });
+
+  const productBasketDiscountWrapper = createElement({ tagName: "div", classNames: ["product-basket__discount-wrapper"] });
+  const productBasketDiscountText = createElement({ tagName: "p", classNames: ["product-basket__small-text"], textContent: "Со скидкой: " });
+  const productBasketDiscount = createElement({ tagName: "p", classNames: ["product-basket__discount"], textContent: `1700 р.` });
+
+  const productBasketFinalPriceWrapper = createElement({ tagName: "div", classNames: ["product-basket__final-price-wrapper"] });
   const productBasketFinalPriceText = createElement({
     tagName: "p",
-    classNames: ["product-busket__small-text"],
+    classNames: ["product-basket__small-text"],
     textContent: `Общая стоимость: `,
   });
-  const productBasketFinalPrice = createElement({ tagName: "p", classNames: ["product-busket__final-price"], textContent: `3400 р.` });
+  const productBasketFinalPrice = createElement({ tagName: "p", classNames: ["product-basket__final-price"], textContent: `3400 р.` });
 
-  proudctBasketAmountWrapper.append(prodcutBasketPlusAmountButton, productBasketAmount, prodcutBasketMinusAmountButton);
-  productBasketPricesWrapper.append(productBasketPriceWrapper, productBasketDiscoutWrapper, productBasketFinalPriceWrapper);
-  prodcutBasketAmountWrapper.append(productBasketAmountText, proudctBasketAmountWrapper);
+  productBasketAmountButtonsWrapper.append(productBasketPlusAmountButton, productBasketAmount, productBasketMinusAmountButton);
+  productBasketPricesWrapper.append(productBasketPriceWrapper, productBasketDiscountWrapper, productBasketFinalPriceWrapper);
+  productBasketAmountWrapper.append(productBasketAmountText, productBasketAmountButtonsWrapper);
   productBasketSizeWrapper.append(productBasketSizeText, productBasketSize);
   productBasketPriceWrapper.append(productBasketPriceText, productBasketPrice);
   productBasketFinalPriceWrapper.append(productBasketFinalPriceText, productBasketFinalPrice);
-  productBasketDiscoutWrapper.append(productBasketDiscoutText, productBasketDiscout);
-  productBasketAmountSizeWrapper.append(prodcutBasketAmountWrapper, productBasketSizeWrapper);
+  productBasketDiscountWrapper.append(productBasketDiscountText, productBasketDiscount);
+  productBasketAmountSizeWrapper.append(productBasketAmountWrapper, productBasketSizeWrapper);
   productBasketTextWrapper.append(productBasketName, productBasketAmountSizeWrapper, productBasketPricesWrapper);
   productBasketDeleteIconWrapper.append(productBasketDeleteIcon);
   productBasketItem.append(productBasketImage, productBasketTextWrapper, productBasketDeleteIconWrapper);
