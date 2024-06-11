85b3df4c0c1469a3e95a1966b63f07ddb398aa05
Darya Pchelintseva <raelin@mail.ru>
2024-06-11 12:20:17 +0300

    MS-141 feat(delete): add delete product from product page

--- src/pages/catalog/modalSize/modalSize.ts
+++ src/pages/catalog/modalSize/modalSize.ts
@@ -2,14 +2,16 @@ import "./modalSize.css";
 import "../../product/product.css";
 import { createElement, createSvgElement } from "../../../components/elements";
 import { cross } from "../../../components/svg";
-import { ClientResponse, ProductProjectionPagedSearchResponse } from "@commercetools/platform-sdk";
+import { ClientResponse, ProductProjectionPagedSearchResponse, Cart } from "@commercetools/platform-sdk";
 import { getCart, updateCart } from "../../../api/api";
-import { getItemFromLocalStorage } from "src/utils/utils";
-import state from "src/store/state";
 
-export function createModalSize(response: ClientResponse<ProductProjectionPagedSearchResponse> | undefined) {
+export function createModalSize(
+  response: ClientResponse<ProductProjectionPagedSearchResponse> | undefined,
+  cartResponse: ClientResponse<Cart> | undefined,
+) {
   const productSizes = response?.body.results[0].variants;
   const productId = response?.body.results[0].id;
+  const itemsInCart = cartResponse?.body.lineItems;
 
   const modalsizeBack = createElement({ tagName: "div", classNames: ["modal-back"] });
   const modalSize = createElement({ tagName: "div", classNames: ["modal"] });
@@ -21,12 +23,28 @@ export function createModalSize(response: ClientResponse<ProductProjectionPagedS
       const sizeVariant = variant.attributes[0].value[0].key;
       const variantId = variant.id;
       const sizeItem = createElement({
-        tagName: "span",
+        tagName: "button",
         classNames: ["product__size-item"],
         textContent: `${sizeVariant}`,
         attributes: { "data-id": `${variantId}` },
       });
       size.append(sizeItem);
+    }
+  });
+
+  itemsInCart?.forEach((item) => {
+    if (productId === item?.productId) {
+      size.querySelectorAll<HTMLButtonElement>(".product__size-item").forEach((sizeItem) => {
+        if (Number(sizeItem.getAttribute("data-id")) === item.variant.id) {
+          sizeItem.classList.add("active");
+          sizeItem.disabled = true;
+          // buyButton.remove();
+          // pricesWrapper.append(deleteButton);
+        } else {
+          sizeItem.disabled = true;
+          sizeItem.classList.add("inactive");
+        }
+      });
     }
   });
 
@@ -39,12 +57,11 @@ export function createModalSize(response: ClientResponse<ProductProjectionPagedS
 
   modalsizeBack.addEventListener("click", async (event) => {
     modalsizeBack.remove();
-    const target = <HTMLSpanElement>event.target;
+    const target = <HTMLButtonElement>event.target;
     if (target.classList.contains("product__size-item")) {
       const variantId = Number(target.getAttribute("data-id"));
       const response = await getCart();
       const version = <number>response?.body.version;
-
       await updateCart(version, [{ action: "addLineItem", productId: `${productId}`, variantId, quantity: 1 }]);
     }
   });
