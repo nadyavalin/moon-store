869bfcd32ae4f241e9e86b985ae16dfca6234762
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-06-12 23:39:30 +0300

    MS-138 feat(sizes modal): make proper styles for sizes modal

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -2,9 +2,16 @@ import "./main.css";
 import createCard from "../../components/productCard/productCard";
 import { getCart, getProducts } from "../../api/api";
 import { createSlider } from "../../components/slider/slider";
+// import state from "../../store/state";
+// import { QueryParam } from "@commercetools/platform-sdk";
 
 export async function getMainPageContent() {
   const response = await getProducts();
+  // let limitCards = response?.body.limit;
+  // limitCards = 20;
+
+  // const getCardsLimit = (queryArgs?: Record<string, QueryParam>) =>
+  //   state.apiRoot?.productProjections().get({ queryArgs: { limit: 20, ...queryArgs } });
   const cartResponse = await getCart();
   const slider = createSlider({
     className: "main-slider",
